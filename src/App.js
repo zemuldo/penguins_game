fb08a86036e376f0ff6d09c9cd8517d9be0af590
Danstan O. Onyango Zemuldo <danstan.otieno@gmail.com>
2018-04-21 17:56:46 +0300

    adds play horizontal

--- src/App.js
+++ src/App.js
@@ -9,29 +9,18 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      width: 10,
-      height: 10,
+      width: this.props.width || 10,
+      height: this.props.height || 10,
       selected: {
         width: 0,
         height: 0
       },
       board: [],
+      unchopped:{},
       chopped: {},
       playW:5,
       playH:5
     }
-  }
-
-  createBord = () => {
-    let rows = []
-    for (let i = 0; i < this.state.width; i++) {
-      let row = []
-      for (let j = 0; j < this.state.width; j++) {
-        row.push(<span className={'bord-box'} ><i className="fa fa-linux" style={{ fontSize: '48px', color: 'blue' }}></i></span>)
-      }
-      rows.push(<div>{row}</div>)
-    }
-    return rows
   }
 
   createBordNumbers = () => {
@@ -45,6 +34,12 @@ class App extends Component {
         for (let x = 0; x < toshow; x++) { playList[randomNos(0, this.state.width - 1)] = true }
 
         for (let j = 0; j < this.state.width; j++) {
+          this.setState(prevState => ({
+            unchopped: {
+                ...prevState.unchopped,
+                [`${i}-${j}`]: true
+            }
+        }))
           row.push({ key: `${i}-${j}`, show: !!playList[j] })
         }
         rows.push(row)
@@ -78,6 +73,10 @@ class App extends Component {
             [`${this.state.playW}-${this.state.playH}`]: true
         }
     }))
+    this.setState(prevState => ({
+      unchopped: delete prevState.unchopped[`${this.state.playW}-${this.state.playH}`]
+  }))
+    
     } else if (e.key === 40 || e.key === 'ArrowDown') {
       if(this.state.playW===this.state.width-1){
         return false
@@ -90,30 +89,30 @@ class App extends Component {
         }
     }))
     }
-    // else if (e.key === 37 || e.key === 'ArrowLeft') {
-    //   if(this.state.playH===0){
-    //     return false
-    //   }
-    //   this.setState({playH:this.state.playH-1})
-    //   this.setState(prevState => ({
-    //     chopped: {
-    //         ...prevState.chopped,
-    //         [`${this.state.playW}-${this.state.playH}`]: true
-    //     }
-    // }))
-    // }
-    // else if (e.key === 39 || e.key === 'ArrowRight') {
-    //   if(this.state.playH===0){
-    //     return false
-    //   }
-    //   this.setState({playH:this.state.playH-1})
-    //   this.setState(prevState => ({
-    //     chopped: {
-    //         ...prevState.chopped,
-    //         [`${this.state.playW}-${this.state.playH}`]: true
-    //     }
-    // }))
-    // }
+    else if (e.key === 37 || e.key === 'ArrowLeft') {
+      if(this.state.playH===0){
+        return false
+      }
+      this.setState({playH:this.state.playH-1})
+      this.setState(prevState => ({
+        chopped: {
+            ...prevState.chopped,
+            [`${this.state.playW}-${this.state.playH}`]: true
+        }
+    }))
+    }
+    else if (e.key === 39 || e.key === 'ArrowRight') {
+      if(this.state.playH===this.state.width-1){
+        return false
+      }
+      this.setState({playH:this.state.playH+1})
+      this.setState(prevState => ({
+        chopped: {
+            ...prevState.chopped,
+            [`${this.state.playW}-${this.state.playH}`]: true
+        }
+    }))
+    }
   }
   getRandom = () => {
     randomNos(0, this.state.width)
@@ -146,6 +145,7 @@ class App extends Component {
     })
     return (
       <div className="App">
+      <p>{`${Object.keys(this.state.unchopped).length} Penguins to chop today`}</p>
         <div className={'board'} onKeyDown={this.playGame}>
           {board}
         </div>
