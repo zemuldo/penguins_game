fdcd3227ed492bbdf65585f1acd9c1dae76b093e
Danstan O. Onyango Zemuldo <danstan.otieno@gmail.com>
2018-04-23 08:01:56 +0300

    adds jest test all components

--- src/App.test.js
+++ src/App.test.js
@@ -2,12 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Board from './game/board';
+import { BrowserRouter } from 'react-router-dom'
+import './index.css';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './store/reducers'
+
+
+const store = createStore(
+    reducers,
+    applyMiddleware(thunk)
+);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+</Provider>, div);
 });
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Board />, div);
+  ReactDOM.render(<Provider store={store}>
+    <BrowserRouter>
+        <Board />
+    </BrowserRouter>
+</Provider>, div);
 });
